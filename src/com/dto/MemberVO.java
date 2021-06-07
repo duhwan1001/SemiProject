0675e0d86f3b6e00bc045551c8ef6282453ac7fa
duhwan1001 <79068555+duhwan1001@users.noreply.github.com>
2021-06-07 19:42:13 +0900

    MemberVO: test commit

--- src/com/dto/MemberVO.java
+++ src/com/dto/MemberVO.java
@@ -1,5 +1,15 @@
 package com.dto;
 
 public class MemberVO {
+	
+	private String memberId;
 
+	public String getMemberId() {
+		return memberId;
+	}
+
+	public void setMemberId(String memberId) {
+		this.memberId = memberId;
+	}
+	
 }
